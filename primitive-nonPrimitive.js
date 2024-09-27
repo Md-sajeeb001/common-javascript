b8085117d96f07c13a8d000dfeeff6f9c90eb84e
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-27 21:18:01 +0800

    first commit

---
+++ primitive-nonPrimitive.js
@@ -0,0 +1,27 @@
+// primitive value gula hocche
+
+// 1. string
+// 2. number
+// 3. boolen
+// 4. null
+// 5. undefiend
+
+const str = 'my name is sajeeb';
+const ageNum = 21;
+const boolen = true;
+
+
+// Non Primitive value gula hocche 
+
+// 1. array 
+// 2. object
+
+const arr = [12, 232, 54, 65];
+let newArr = arr;
+// set new array !
+newArr = [33, 44, 55];
+// console.log(newArr);
+
+const frnArr = ['shohin', 'kohin', 'jamin'];
+const result = frnArr;
+console.log(result);
---
+++ true-false.js
@@ -0,0 +1,22 @@
+// check true
+
+// 1. true
+// 2. number is turty value
+// 3. string , empty string with space is a turty value
+// 4. array , empty array is a truty value
+// 5. object, empty object is also truty value
+
+// check false value
+// 1. false
+// 2. empty string is falsy value
+// 3. number 0 is false value
+// 4. null is a falsy value
+// 5. undefiend is a falsy value
+
+const x = [];
+if(x){
+    console.log('value is truty');
+}
+else{
+    console.log('value is falsy');
+}
